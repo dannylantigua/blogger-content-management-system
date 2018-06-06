5666ccc8be39693707796b39914020479765e7f9
Your Name <jyohuali@gmail.com>
2018-06-05 21:02:49 -0400

    dao

---
+++ BlogCMS/src/main/java/com/sg/blogcms/daoImpl/PostDaoJdbcTemplateImpl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.blogcms.daoImpl;
+
+import com.sg.blogcms.dao.PostsDao;
+import com.sg.blogcms.model.Posts;
+
+/**
+ *
+ * @author jonathan
+ *
+ */
+public class PostDaoJdbcTemplateImpl implements PostsDao {
+
+    @Override
+    public void createPost(Posts currentPosts) {
+       
+    }
+    
+}
